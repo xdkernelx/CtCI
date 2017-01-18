7549db39f5333bd089d1473920ac0e9c87d18a73
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-17 20:03:53 -0800

    Fix critical error not returning the position of the laser.

--- LaserMazeSolver.java
+++ LaserMazeSolver.java
@@ -5,28 +5,29 @@ public class LaserMazeSolver {
     public static final int NEST_COL = 1;
     public static final String BEAM_SIGNAL = "@";
 
-    // public static void findStartPosition(int[] position, String[][] maze) {
+    public static int[] findStartPosition(String[][] maze) {
 
-    //     int row_size = maze[0].length - 1;
+        int row_size = maze[0].length - 1;
+        int[] position = new int[2];
 
-    //     for (int i = 0; i < MAX_ROW; i++) {
-    //         System.out.println(i);  
-    //         for (int j = 0; j < row_size; j++) {
-    //             if (maze[i][j] == BEAM_SIGNAL) {
-    //                 position[0] = i;
-    //                 position[1] = j;    
-    //             }
-    //         }
-    //     }
+        for (int i = 0; i < MAX_ROW; i++) {
+            for (int j = 0; j < row_size; j++) {
+                if (maze[i][j].equals(BEAM_SIGNAL)) {
+                    position[0] = i;
+                    position[1] = j;
+                    return position;    
+                }
+            }
+        }
 
-
-    // }
+        return position;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[][] maze = new String[MAX_ROW][NEST_COL];
         boolean first_line = true;
-        int[] position = new int[2];
+        int[] position = null;
         int index = 0;
 
         while (scanner.hasNextLine()) {
@@ -36,7 +37,8 @@ public class LaserMazeSolver {
             index++;
         }
 
-        // findStartPosition(position, maze);
+        position = findStartPosition(maze);
+        System.out.println(position);
 
 
     }
